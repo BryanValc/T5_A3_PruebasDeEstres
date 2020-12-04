9efcb27e89ed8ef7eac576debb08afa16a84a873
Bryan Valdez <bryan501th@gmail.com>
2020-12-04 17:12:09 -0600

    Se agregó la clase de Intercalación

--- src/PruebaPruebasDeEstres.java
+++ src/PruebaPruebasDeEstres.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class GeneracionNumeros{
 	public static int[] generarNumerosAleatorios(int cnt){
 		int[] ret = new int[cnt];
@@ -9,7 +11,7 @@ class GeneracionNumeros{
 }
 
 class MetodosOrdenamiento{
-	static long resultados[][][]=new long[4][8][4];
+	static long resultados[][][]=new long[4][11][4];
 	static class Burbuja {
 		
 		public static void ordenacionBurbuja1(int nums[],int np) {
@@ -312,13 +314,92 @@ class MetodosOrdenamiento{
 	}//class Radix
 	
 	
+	static class Intercalacion{
+		
+		public static void complementoIntercalacion(int numeros[]){
+			int aux;
+			for (int i = 1; i < numeros.length; i++) {
+				aux=numeros[i];
+				for (int j=i-1; j>=0 && numeros[j]>aux ; j--) {
+					numeros[j+1]=numeros[j];
+					numeros[j]=aux;
+				}
+			}
+		}
+		
+		public static void ordenacionIntercalacion(int nums[],int np) {
+			int numeros[]=nums.clone();
+			long comparaciones=0,intercambios=0,recorridos=0;
+			
+			int[] arregloA,arregloB;
+			
+			if (numeros.length%2==0) {
+				arregloA=new int[numeros.length/2];
+				arregloB=new int[numeros.length/2];
+			}else {
+				arregloA=new int[numeros.length/2];
+				arregloB=new int[(numeros.length/2)+1];
+			}
+			for (int i = 0; i < numeros.length; i++) {
+				if (i<numeros.length/2) {
+					arregloA[i]=numeros[i];
+				}else {
+					arregloB[i-arregloA.length]=numeros[i];
+				}
+			}
+			complementoIntercalacion(arregloA);
+			complementoIntercalacion(arregloB);
+			
+			long ini = System.nanoTime();
+			
+			int arregloC[] = new int[arregloA.length+arregloB.length];
+			
+			int i,k,j;
+			recorridos+=1;
+			for(i=j=k=0; i<arregloA.length && j<arregloB.length; k++){
+				comparaciones+=1;
+				intercambios+=1;
+				if(arregloA[i]<arregloB[j]) {
+					arregloC[k] = arregloA[i];
+					i++;
+				}else {
+					arregloC[k] = arregloB[j];
+					j++;
+				}
+			}
+			recorridos+=1;
+			for(;i<arregloA.length; i++,k++) {
+				arregloC[k] = arregloA[i];
+				intercambios+=1;
+			}
+			recorridos+=1;
+			for(;j<arregloB.length; j++,k++) {
+				arregloC[k] = arregloB[j];
+				intercambios+=1;
+			}
+			
+			long fin = System.nanoTime();
+			
+			resultados[np][8][0]=comparaciones;
+			resultados[np][8][1]=intercambios;
+			resultados[np][8][2]=recorridos;
+			resultados[np][8][3]=fin-ini;
+		}
+		
+	}//class Intercalacion
+	
+	
+	
+	
+	
+	
 }
 
 public class PruebaPruebasDeEstres {
 
 	public static void main(String[] args) {
 		
-		String metodos[]= {"Burbuja1","Burbuja2","Burbuja3","Insercion","Seleccion","Quicksort","Shellsort","Radix	"};
+		String metodos[]= {"Burbuja1","Burbuja2","Burbuja3","Insercion","Seleccion","Quicksort","Shellsort","Radix	","Intercalacion","Mezcla directo","Mezcla natural"};
 		for (int i = 0; i < 4; i++) {
 			int nums[]=GeneracionNumeros.generarNumerosAleatorios((int) (1000*Math.pow(10, i)));
 			MetodosOrdenamiento.Burbuja.ordenacionBurbuja1(nums, i);
@@ -337,8 +418,10 @@ public class PruebaPruebasDeEstres {
 			System.out.println("Shellsort	"+nums.length+" numeros terminada...");
 			MetodosOrdenamiento.Radix.radix(nums, i);
 			System.out.println("Radix		"+nums.length+" numeros terminada...");
+			MetodosOrdenamiento.Intercalacion.ordenacionIntercalacion(nums, i);
+			System.out.println("Itercalacion"+nums.length+" numeros terminada...");
 			System.out.println("========================prueba de "+nums.length+" numeros========================\nmetodo		|comparaciones	|intercambios	|recorridos	|runtime	|");
-			for (int j = 0; j < 8; j++) {
+			for (int j = 0; j < 11; j++) {
 				System.out.print(metodos[j]+"	|");
 				for (int k = 0; k < 4; k++) {
 					System.out.print(MetodosOrdenamiento.resultados[i][j][k]+(MetodosOrdenamiento.resultados[i][j][k]<1000000 ? "		|":"	|"));
