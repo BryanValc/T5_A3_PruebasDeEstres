2bad4561c2b08dafbcce3c82efc615bcca6524f0
Bryan Valdez <bryan501th@gmail.com>
2020-12-04 17:26:16 -0600

    Se agregaron las clases faltantes

--- src/PruebaPruebasDeEstres.java
+++ src/PruebaPruebasDeEstres.java
@@ -88,8 +88,243 @@ class MetodosOrdenamiento{
 	}//class Intercalacion
 	
 	
+	static class MezclaDirecto{
+		static long comparaciones=0;
+		static long intercambios=0;
+		static long recorridos=0;
+		
+		public static int [] ordenamientoMezclaDirecto(int arreglo[]) {
+			int i,j,k;
+			if(arreglo.length>1) {
+				int numElementosIzq=arreglo.length/2;
+				int numElmentosDer=arreglo.length-numElementosIzq;
+				
+				int arregloIzquierdo[]=new int[numElementosIzq];
+				int arregloDerecha[]=new int[numElmentosDer];
+				recorridos+=1;
+				for(i=0;i<numElementosIzq;i++) {
+					arregloIzquierdo[i]=arreglo[i];
+					intercambios+=1;
+				}
+				i=0;
+				recorridos+=1;
+				for(i=numElementosIzq;i<numElementosIzq+numElmentosDer;i++) {
+					arregloDerecha[i-numElementosIzq]=arreglo[i];
+					intercambios+=1;
+				}
+				
+				arregloIzquierdo=ordenamientoMezclaDirecto(arregloIzquierdo);
+				arregloDerecha=ordenamientoMezclaDirecto(arregloDerecha);
+				i=j=k=0;
+				recorridos+=1;
+				while(arregloIzquierdo.length!=j && arregloDerecha.length!=k) {
+					comparaciones+=1;
+					intercambios+=1;
+					if(arregloIzquierdo[j]<arregloDerecha[k]) {
+						arreglo[i]=arregloIzquierdo[j];
+						i++;
+						j++;
+					}else {
+						arreglo[i]=arregloDerecha[k];
+						i++;
+						k++;
+					}
+				}
+				recorridos+=1;
+				while(arregloIzquierdo.length!=j) {
+					intercambios+=1;
+					arreglo[i]=arregloIzquierdo[j];
+					i++;
+					j++;
+				}
+				recorridos+=1;
+				while(arregloDerecha.length!=k) {
+					intercambios+=1;
+					arreglo[i]=arregloDerecha[k];
+					i++;
+					k++;
+				}
+			}
+			
+			return arreglo;
+		}
+		
+		public static void llamadaOrdenamientoMezclaDirecto(int nums[],int np) {
+			int numeros[]=nums.clone();
+			
+			long ini = System.nanoTime();
+			ordenamientoMezclaDirecto(numeros);
+			long fin = System.nanoTime();
+			
+			resultados[np][1][0]=comparaciones;
+			resultados[np][1][1]=intercambios;
+			resultados[np][1][2]=recorridos;
+			resultados[np][1][3]=fin-ini;
+			comparaciones=intercambios=recorridos=0;
+		}
+
+	}
 	
 	
+	static class MezclaNatural {
+		static long comparaciones=0;
+		static long intercambios=0;
+		static long recorridos=0;
+		
+		public static int[] mezclaDirecta (int[] vector) {
+			int i,j,k;
+			if(vector.length>1) {
+				int nElementosIzquierda = vector.length/2;
+				int nElementosDerecha = vector.length - nElementosIzquierda;
+				int vectorI[] = new int [nElementosIzquierda];
+				int vectorD[] = new int [nElementosDerecha];
+				recorridos+=1;
+				for (i = 0; i < nElementosIzquierda; i++) {
+					intercambios+=1;
+					vectorI[i]=vector[i];
+				}
+				recorridos+=1;
+				for (i = nElementosIzquierda; i < nElementosIzquierda+nElementosDerecha; i++){
+					intercambios+=1;
+					vectorD[i-nElementosIzquierda] = vector[i];
+				}
+				vectorI = mezclaDirecta(vectorI);
+				vectorD = mezclaDirecta(vectorD);
+				i=0;
+				j=0;
+				k=0;
+				recorridos+=1;
+				while(vectorI.length!=j && vectorD.length!=k) {
+					intercambios+=1;
+					comparaciones+=1;
+					if(vectorI[j]<vectorD[k]) {
+						vector[i]=vectorI[j];
+						i++;
+						j++;
+					}else {
+						vector[i]=vectorD[k];
+						i++;
+						k++;
+					}//Else
+				}//While
+				recorridos+=1;
+				while(vectorI.length!=j) {
+					intercambios+=1;
+					vector[i] = vectorI[j];
+					i++;
+					j++;
+				}
+				recorridos+=1;
+				while(vectorD.length!=k) {
+					intercambios+=1;
+					vector[i] = vectorD[k];
+					i++;
+					k++;
+				}	
+			}
+			return vector;
+		}
+		//MezclaDirecta2
+		public static void mezclaDirecta2 (int[] vector) {
+			int i,j,k;
+			if(vector.length>1) {
+				int nElementosIzquierda = vector.length/2;
+				int nElementosDerecha = vector.length - nElementosIzquierda;
+				int vectorI[] = new int [nElementosIzquierda];
+				int vectorD[] = new int [nElementosDerecha];
+				recorridos+=1;
+				for (i = 0; i < nElementosIzquierda; i++) {
+					intercambios+=1;
+					vectorI[i]=vector[i];
+				}
+				recorridos+=1;
+				for (i = nElementosIzquierda; i < nElementosIzquierda+nElementosDerecha; i++){
+					intercambios+=1;
+					vectorD[i-nElementosIzquierda] = vector[i];
+				}
+				vectorI = mezclaDirecta(vectorI);
+				vectorD = mezclaDirecta(vectorD);
+				i=0;
+				j=0;
+				k=0;
+				recorridos+=1;
+				while(vectorI.length!=j && vectorD.length!=k) {
+					comparaciones+=1;
+					if(vectorI[j]<vectorD[k]) {
+						vector[i]=vectorI[j];
+						i++;
+						j++;
+					}else {
+						vector[i]=vectorD[k];
+						i++;
+						k++;
+					}//Else
+				}//While
+				recorridos+=1;
+				while(vectorI.length!=j) {
+					intercambios+=1;
+					vector[i] = vectorI[j];
+					i++;
+					j++;
+				}
+				recorridos+=1;
+				while(vectorD.length!=k) {
+					intercambios+=1;
+					vector[i] = vectorD[k];
+					i++;
+					k++;
+				}	
+			}
+		}
+		
+		public static void mezclaNatural(int[] nums,int np) {
+			int numeros[]= nums.clone();
+			
+			int izquierda =0;
+			int izq =0;
+			int derecha = numeros.length-1;//Sin el error de nullPointerExepcion
+			int der = derecha;
+			boolean ordenado = false;
+			
+			long ini = System.nanoTime();
+			do {
+				ordenado = true;
+				izquierda = 0;
+				while(izquierda<derecha) {
+					izq =izquierda;
+					recorridos+=1;
+					while(izq < derecha && numeros[izq]<=numeros[izq+1]) {
+						izq++;
+					}
+					der = izq +1;
+					recorridos+=1;
+					while(der==derecha-1 || der<derecha && numeros[der]<=numeros[der+1]) {
+						der++;
+					}
+					comparaciones+=1;
+					if(der<=derecha) {
+						mezclaDirecta2(numeros);
+						
+						ordenado = false;
+					}
+					izquierda=izq;
+					
+				}
+			}while(!ordenado);
+			long fin = System.nanoTime();
+			
+			resultados[np][2][0]=comparaciones;
+			resultados[np][2][1]=intercambios;
+			resultados[np][2][2]=recorridos;
+			resultados[np][2][3]=fin-ini;
+			
+			comparaciones=intercambios=recorridos=0;
+			
+			
+		
+		}
+		
+	}//Mezcla natural
 	
 	
 }
@@ -102,6 +337,8 @@ public class PruebaPruebasDeEstres {
 		for (int i = 0; i < 4; i++) {
 			int nums[]=GeneracionNumeros.generarNumerosAleatorios((int) (1000*Math.pow(10, i)));
 			MetodosOrdenamiento.Intercalacion.ordenacionIntercalacion(nums, i);
+			MetodosOrdenamiento.MezclaDirecto.llamadaOrdenamientoMezclaDirecto(nums, i);
+			MetodosOrdenamiento.MezclaNatural.mezclaNatural(nums, i);
 			
 			System.out.println("========================prueba de "+nums.length+" numeros========================\nmetodo		|comparaciones	|intercambios	|recorridos	|runtime	|");
 			for (int j = 0; j < 3; j++) {
